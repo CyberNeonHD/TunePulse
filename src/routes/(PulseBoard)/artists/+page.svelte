273882bed1c3480d65a540c8c7a342ac410262d2
kobbe.vereecken <kobbe.vereecken@student.howest.be>
2024-05-19 05:57:42 +0200

    Added pages

---
+++ src/routes/(PulseBoard)/artists/+page.svelte
@@ -0,0 +1,10 @@
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:300, 400, 700&display=swap');
+  /* Add Tailwind CSS styles here or import them as a separate file */
+</style>
+
+<main>
+  <section class="text-center pt-14">
+    <h2 class="text-4xl font-semibold text-white mb-5 animate-jump-in animate-delay-[900ms]">Your top <span style="color: rgb(20, 220, 80);">50</span> artists</h2>
+</main>
+  
