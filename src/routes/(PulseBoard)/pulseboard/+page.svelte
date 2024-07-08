bb36fc31bd9ebd1d9b3f318940b9c19268f11712
CyberNeon <github.ducktail293@passinbox.com>
2024-07-08 14:46:50 -0700

    Redesign of pulseboard patch notes

--- src/routes/(PulseBoard)/pulseboard/+page.svelte
+++ src/routes/(PulseBoard)/pulseboard/+page.svelte
@@ -29,34 +29,46 @@
     <div class="green p-8 rounded-md">
       <h2 class="text-4xl font-semibold">Welcome to the PulseBoard</h2>
       <h3 class="text-3xl font-semibold mb-3">WIP: Hobby Project</h3>
-      <br>
-      <p class="text-xl text-white leading-6">
-        Update 08/07/2024: <br>
-        - Auth bypass fixed<br>
-        - Top 10 Albums now avaible (based on top 50 all-time tracks) <br>
-        <br>
-
-        Update 07/07/2024: <br>
-        - API bug has been fixed. <br>
-        - Data is now being pulled and displayed correctly. <br>
-        - Home page is now mobile friendly. <br>
-        - New features are coming soon! <br>
-        <br>
-
-        Notice 30/06/2024: <br>
-        - Currently there is a bug with pulling the data from the API.<br>
-        - It only pulls the data the first time, then it wipes itself and doesn't re-pull.<br>
-        - For now strackifyer.co.uk is still avaible for use.<br>
-        - I am working on a fix for this issue.<br>
-        <br>
-
-        News 30/06/2024: <br>
-        - Welcome to the beta release of Strackifyer 2.0, better called TunePulse!<br>
-        - Currently in beta, for a couple of weeks. <br>
-        - Please report any bugs or issues you encounter to mail@cyberneon.be.<br>
-        - Better desing and new features are coming soon! Stay tuned! <br>
-        <br>
-      </p>
+      <div class="max-w-3xl mx-auto p-4 shadow-lg rounded-lg">
+        <div class="mb-4">
+            <h2 class="text-xl font-semibold mb-2">Update 08/07/2024:</h2>
+            <ul class="list-disc pl-6">
+                <li>Auth bypass fixed.</li>
+                <li>Top 10 Albums now available (based on top 50 all-time tracks).</li>
+            </ul>
+        </div>
+        
+        <div class="mb-4">
+            <h2 class="text-xl font-semibold mb-2">Update 07/07/2024:</h2>
+            <ul class="list-disc pl-6">
+                <li>API bug has been fixed.</li>
+                <li>Data is now being pulled and displayed correctly.</li>
+                <li>Home page is now mobile friendly.</li>
+                <li>New features are coming soon!</li>
+            </ul>
+        </div>
+        
+        <div class="mb-4">
+            <h2 class="text-xl font-semibold mb-2">Notice 30/06/2024:</h2>
+            <ul class="list-disc pl-6">
+                <li>Currently there is a bug with pulling the data from the API.</li>
+                <li>It only pulls the data the first time, then it wipes itself and doesn't re-pull.</li>
+                <li>For now strackifyer.co.uk is still available for use.</li>
+                <li>I am working on a fix for this issue.</li>
+            </ul>
+        </div>
+        
+        <div class="mb-4">
+            <h2 class="text-xl font-semibold mb-2">News 30/06/2024:</h2>
+            <ul class="list-disc pl-6">
+                <li>Welcome to the beta release of Strackifyer 2.0, better called TunePulse!</li>
+                <li>Currently in beta, for a couple of weeks.</li>
+                <li>Please report any bugs or issues you encounter to <a href="mailto:mail@cyberneon.be" class="text-green-500">mail@cyberneon.be</a>.</li>
+                <li>Or you can report them on the <a class="text-green-500" href="https://github.com/CyberNeonHD/TunePulse">github repo</a>.</li>
+                <li>Better design and new features are coming soon! Stay tuned!</li>
+            </ul>
+        </div>
+      </div>
     </div>
 </section>
 <!-- PulseBoard -->
