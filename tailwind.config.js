31313e3e223e4ea07521e239a07c89425a8a30fa
CyberNeon <github.ducktail293@passinbox.com>
2025-05-30 01:25:48 +0200

    Tailwind and skeleton upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ import { join } from 'path'
 
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-import { skeleton } from '@skeletonlabs/tw-plugin'
-import { tunepulseTheme } from './tunepulse-theme.js'
+
 /** @type {import('tailwindcss').Config} */
+
 export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
@@ -15,26 +15,5 @@ export default {
 		require('tailwindcss-animated'),
 		forms,
 		typography,
-		skeleton({
-			themes: {
-				preset: [
-					{
-						name: 'skeleton',
-						enhancements: true,
-					},
-					{
-						name: 'wintry',
-						enhancements: true,
-					},
-					{
-						name: 'rocket',
-						enhancements: true,
-					},
-				],
-				custom: [
-					tunepulseTheme
-				]
-			},
-		}),
 	],
 };
